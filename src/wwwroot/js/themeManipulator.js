b1670dcf79f13093d226545c56bc377285e5a103
Aeuke Peer Conradi <aeuke-peer@t-online.de>
2023-02-20 23:33:17 +0100

    Updated to bootstrap version 5.2.3

---
+++ src/wwwroot/js/themeManipulator.js
@@ -0,0 +1,4 @@
+﻿function setCssVariable(variableName, newValue) {
+    document.documentElement.style.setProperty(`--${variableName}`, newValue);
+    //alert('Called with: ' + variableName + ' = ' + newValue)
+}
